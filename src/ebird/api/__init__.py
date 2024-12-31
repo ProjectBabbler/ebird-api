7f9172acd12a47eb64cf01b3c26956c1d99e572d
Stuart MacKay <smackay@flagstonesoftware.com>
2024-12-31 08:54:55 +0000

    New version: 3.2.0 → 3.2.1

--- src/ebird/api/__init__.py
+++ src/ebird/api/__init__.py
@@ -2,7 +2,7 @@
 
 """A set of wrapper functions for accessing the eBird API."""
 
-__version__ = "3.2.0"
+__version__ = "3.2.1"
 
 from ebird.api.checklists import get_checklist, get_visits
 from ebird.api.client import Client
